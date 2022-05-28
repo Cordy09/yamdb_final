f207abbab3e9da6baf7f30ba2e8b382661a33ca9
Anna <cordy120993@gmail.com>
2022-05-28 22:02:11 +0000

    flake

--- api_yamdb/reviews/management/commands/csv.py
+++ api_yamdb/reviews/management/commands/csv.py
@@ -1,8 +1,7 @@
 import csv
 
 from django.core.management.base import BaseCommand
-
-from reviews.models import Title, Review, Genre, Category, Comment, User
+from reviews.models import Category, Comment, Genre, Review, Title, User
 
 
 class Command(BaseCommand):
